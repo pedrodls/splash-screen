74f1a3a967c54c7eee43915c7e31620b89025cda
PedroSousa2020 <sousapedro747@gmail.com>
2021-05-11 23:59:09 +0100

    17º exercicio enviado

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Timer timer = new Timer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+
+                startActivity(intent);
+
+                finish();
+            }
+        }, 2000);
+    }
+
+}
